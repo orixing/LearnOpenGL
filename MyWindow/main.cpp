f2ed5ac971ad808ba37f166d002ae3403f5bc3af
xingyongzhe <xingyongzhe@gyyx.cn>
2024-04-23 12:28:27 +0800

    uniform 随时间颜色变化

--- MyWindow/main.cpp
+++ MyWindow/main.cpp
@@ -41,9 +41,11 @@ int main(void)
 
     const char* vertexShaderSource = "#version 330 core\n"
         "layout (location = 0) in vec3 aPos;\n"
+        "//out vec4 vertexColor;\n"
         "void main()\n"
         "{\n"
-        "   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+        "   gl_Position = vec4(aPos, 1.0);\n"
+        "   //vertexColor = vec4(0.5,0.0,0.0,1.0);\n"
         "}\0";
     unsigned int vertexShader;
     vertexShader = glCreateShader(GL_VERTEX_SHADER);
@@ -52,9 +54,11 @@ int main(void)
 
     const char* fragmentShaderSource = "#version 330 core\n"
         "out vec4 FragColor;\n"
+        "//in vec4 vertexColor;\n"
+        "uniform vec4 ourColor;"
         "void main()\n"
         "{\n"
-        "FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
+        "FragColor = ourColor;\n"
         "}\n";
     unsigned int fragmentShader;
     fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
@@ -65,7 +69,7 @@ int main(void)
         "out vec4 FragColor;\n"
         "void main()\n"
         "{\n"
-        "FragColor = vec4(0.0f, 0.1f, 0.0f, 1.0f);\n"
+        "FragColor = vec4(0.0f, 0.5f, 0.0f, 1.0f);\n"
         "}\n";
     unsigned int fragmentGreenShader;
     fragmentGreenShader = glCreateShader(GL_FRAGMENT_SHADER);
@@ -134,7 +138,12 @@ int main(void)
         //glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
         //glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
 
+        float timeValue = glfwGetTime();
+        float greenValue = (sin(timeValue) / 2.0f) + 0.5f;
+        int vertexColorLocation = glGetUniformLocation(shaderProgram, "ourColor");
         glUseProgram(shaderProgram);
+        glUniform4f(vertexColorLocation, 0.0f, greenValue, 0.0f, 1.0f);
+
         glBindVertexArray(VAO[0]);
         glDrawArrays(GL_TRIANGLES, 0, 3);
         glUseProgram(greenShaderProgram);
