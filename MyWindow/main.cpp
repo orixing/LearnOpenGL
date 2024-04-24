f1a27806333b1f676478ed38b30438d4061552d7
xingyongzhe <xingyongzhe@gyyx.cn>
2024-04-24 20:22:39 +0800

    [摄像机]使用lookat

--- MyWindow/main.cpp
+++ MyWindow/main.cpp
@@ -230,18 +230,8 @@ int main(void)
         float radius = 10.0f;
         float camX = sin(glfwGetTime()) * radius;
         float camZ = cos(glfwGetTime()) * radius;
-        //glm::mat4 view;
-        glm::vec3 cameraPos = glm::vec3(camX, 0.0, camZ);
-        glm::vec3 cameraDirc = glm::normalize(glm::vec3(0.0, 0.0, 0.0) - cameraPos);
-        glm::vec3 cameraUp = glm::vec3(0.0, 1.0, 0.0);
-        glm::vec3 cameraRight = glm::normalize(glm::cross(cameraDirc, cameraUp));
-        glm::mat4 view(cameraRight.x, cameraUp.x, -cameraDirc.x, 0.0,
-            cameraRight.y, cameraUp.y, -cameraDirc.y, 0.0,
-            cameraRight.z, cameraUp.z, -cameraDirc.z, 0.0,
-            0.0, 0.0, 0.0, 1.0);
-
-        view = glm::translate(view, -cameraPos);
-        //view = glm::lookAt(glm::vec3(camX, 0.0, camZ), glm::vec3(0.0, 0.0, 0.0), glm::vec3(0.0, 1.0, 0.0));
+        glm::mat4 view;
+        view = glm::lookAt(glm::vec3(camX, 0.0, camZ), glm::vec3(0.0, 0.0, 0.0), glm::vec3(0.0, 1.0, 0.0));
         glm::mat4 projection;
         projection = glm::perspective(glm::radians(45.0f), screenWidth / screenHeight, 0.1f, 100.0f);
 
