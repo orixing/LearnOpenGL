cb21326af14c4c9576d9a93b61125a2c8bbeae59
xingyongzhe <xingyongzhe@gyyx.cn>
2024-05-06 14:00:26 +0800

    [面剔除]去除正面

--- MyWindow/main.cpp
+++ MyWindow/main.cpp
@@ -141,6 +141,9 @@ int main(void)
     glEnable(GL_BLEND);
     glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
 
+    glEnable(GL_CULL_FACE);
+    glCullFace(GL_FRONT);
+
     while (!glfwWindowShouldClose(window))
     {
         processInput(window);
