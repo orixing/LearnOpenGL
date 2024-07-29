c9af2918125e9185f742c571c6ce4da0cb911050
邢湧喆 <orixyz@163.com>
2024-07-29 23:19:04 +0800

    修改FXAA

--- MyWindow/shaders/FXAAFragmentShader.fs
+++ MyWindow/shaders/FXAAFragmentShader.fs
@@ -39,13 +39,11 @@ void main() {
         return;
     }
 
-    float pixelContrast = abs(lumaM - 0.25*(lumaN+lumaW+lumaS+lumaE));
+    //float pixelContrast = abs(lumaM - 0.25*(lumaN+lumaW+lumaS+lumaE));
     //pixel contrast与local contrast的比值越接近1说明越像是噪点
 
-    float blendL = max(0.0, (pixelContrast / localContrast) - 0.25);
-    blendL = min(0.75, blendL); 
-
-  
+    //float blendL = max(0.0, (pixelContrast / localContrast) - 0.25);
+    //blendL = min(0.75, blendL); 
 
     vec3 rgbNW = texture(screenTexture, TexCoords + (vec2(-1.0, -1.0) * texelSize)).xyz;
     vec3 rgbNE = texture(screenTexture, TexCoords + (vec2(1.0, -1.0) * texelSize)).xyz;
@@ -67,10 +65,11 @@ void main() {
         abs((0.25 * lumaNE) + (-0.5 * lumaE) + (0.25 * lumaSE));
     bool horzSpan = edgeHorz >= edgeVert; 
 
+    //遍历时的步长
     vec2 texStep = horzSpan ? vec2(1.0,0.0) : vec2(0.0,1.0);
     texStep *= texelSize;
     //正反方向寻找边界是否结束
-    //确定要向两侧遍历的像素对起点
+    //确定要向两侧遍历的像素对起点，一个是本像素，一个是锯齿另一侧的像素
     vec2 pos1 = TexCoords, pos2;
     if(horzSpan){
         pos2 = (abs(lumaM - lumaN)>abs(lumaM - lumaS))? TexCoords + (vec2(0.0, -1.0) * texelSize) : TexCoords + (vec2(0.0, 1.0) * texelSize);
@@ -78,56 +77,70 @@ void main() {
         pos2 = (abs(lumaM - lumaW)>abs(lumaM - lumaE))? TexCoords + (vec2(-1.0, 0.0) * texelSize) : TexCoords + (vec2(1.0, 0.0) * texelSize);
     }
 
-    //开始向两侧遍历
+    //起始像素对 两像素的亮度，一个是lumaM
+    float lumaOther = dot(texture(screenTexture, pos2).xyz, vec3(0.299, 0.587, 0.114));
+
+    //开始向两侧遍历 N负方向 P正方向
     float lumaEndN1 ,lumaEndN2, lumaEndP1,lumaEndP2;
     float countN = 0.0,countP = 0.0;
-    bool nearMN= false, nearMP = false;//边界结束的地方是内侧还是外侧
-    for(int i = 0; i < 20 ; i++)
+    bool nearMN= false, nearMP = false;//边界结束的地方是内侧(和M同色)还是外侧(和Other同色)
+    for(int i = 0; i < 100 ; i++)
     {   
-        vec3 rgbEndN1 = texture(screenTexture, pos1 + texStep * i).xyz;
+        //待测的一对像素对
+        vec3 rgbEndN1 = texture(screenTexture, pos1 - texStep * i).xyz;
         float lumaEndN1 = dot(rgbEndN1, vec3(0.299, 0.587, 0.114));
-        vec3 rgbEndN2 = texture(screenTexture, pos2 + texStep * i).xyz;
+        vec3 rgbEndN2 = texture(screenTexture, pos2 - texStep * i).xyz;
         float lumaEndN2 = dot(rgbEndN2, vec3(0.299, 0.587, 0.114));
-        //如果两个像素差距较小，认为边界到头了，如果原采样点lumaM和对应的lumaEnd1差距大，也认为边界到头了
-        if(abs(lumaEndN1 - lumaEndN2) < 0.25 || abs(lumaEndN1 - lumaM)>0.25){
+        //如果出现和原始像素对差异较大的颜色，则这段可能不是锯齿
+        if(abs(lumaEndN1 - lumaM) > 0.15 || abs(lumaEndN2 - lumaM) > 0.15 || abs(lumaEndN1 - lumaOther) > 0.15 || abs(lumaEndN2 - lumaOther) > 0.15){
+            FragColor = vec4(rgbM,1.0);
+            return;
+        }
+        //如果两个像素差距较小，认为边界到头了
+        if(abs(lumaEndN1 - lumaEndN2) < 0.1){
             //两个点都接近lumaM，表示在内侧
-            if(abs(lumaM - lumaEndN2) < 0.25  && abs(lumaEndN1 - lumaM) < 0.25 ){
-                nearMN = true;
-            }//否则在外侧
+            //否则在外侧, nearMN = false
+            nearMN = abs(lumaM - lumaEndN2) < 0.15  && abs(lumaEndN1 - lumaM) < 0.15;
+            countN = i + 1;
             break;
         }
-        countN +=1.0;
     }
 
-    for(int i = 0; i < 20 ; i++)
+    for(int i = 0; i < 100 ; i++)
     {   
-        vec3 rgbEndP1 = texture(screenTexture, pos1 - texStep * i).xyz;
+        vec3 rgbEndP1 = texture(screenTexture, pos1 + texStep * i).xyz;
         float lumaEndP1 = dot(rgbEndP1, vec3(0.299, 0.587, 0.114));
-        vec3 rgbEndP2 = texture(screenTexture, pos2 - texStep * i).xyz;
+        vec3 rgbEndP2 = texture(screenTexture, pos2 + texStep * i).xyz;
         float lumaEndP2 = dot(rgbEndP2, vec3(0.299, 0.587, 0.114));
-        //如果两个像素差距较小，认为边界到头了，如果原采样点lumaM和对应的lumaEnd1差距大，也认为边界到头了
-        if(abs(lumaEndP1 - lumaEndP2) < 0.25 || abs(lumaEndP1 - lumaM)>0.25){
-            //两个点都接近lumaM，表示在内侧
-            if(abs(lumaM - lumaEndP1) < 0.25  && abs(lumaEndP1 - lumaM) < 0.25 ){
-                nearMP = true;
-            }//否则在外侧
+        //如果出现和原始像素对差异较大的颜色，则这段可能不是锯齿
+        if(abs(lumaEndP1 - lumaM) > 0.15 || abs(lumaEndP2 - lumaM) > 0.15 || abs(lumaEndP1 - lumaOther) > 0.15 || abs(lumaEndP2 - lumaOther) > 0.15){
+            FragColor = vec4(rgbM,1.0);
+            return;
+        }
+        //如果两个像素差距较小，认为边界到头了
+        if(abs(lumaEndP1 - lumaEndP2) < 0.1){
+            //两个点都接近lumaM，表示在内侧,否则在外侧
+            nearMP = abs(lumaM - lumaEndP1) < 0.15  && abs(lumaEndP1 - lumaM) < 0.15;
+            countP = i + 1;
             break;
         }
-        countP +=1.0;
     }
 
-    float totalL = countN + countP + 1.0;
-
+    //某一侧达到遍历上限
+    if(countN < 0.001 || countP < 0.001){
+        FragColor = vec4(rgbM,1.0);
+        return;
+    }
     //如果同侧，暂不处理
     if(nearMP == nearMN){
         FragColor = vec4(rgbM,1.0);
         return;
     }
     if(nearMN){
-        vec2 pos = ((countP + 1.0) * pos1 + (countN + 1.0) * pos2)/  (totalL+1.0);
+        vec2 pos = (countP * pos1 + countN * pos2) /  (countN + countP);
         FragColor = vec4(textureLod(screenTexture, pos, 0.0).xyz,1.0);
     } else{
-        vec2 pos = ((countN + 1.0) * pos1 + (countP + 1.0) * pos2)/  (totalL+1.0);
+        vec2 pos = (countN * pos1 + countP * pos2) /  (countN + countP);
         FragColor = vec4(textureLod(screenTexture, pos, 0.0).xyz,1.0);
     }
 
